e8799f8837d73273665df569e8794573bcec617d
Connor Peet <connor@peet.io>
2015-03-31 12:20:05 -0400

    Throw an informative error on missing modules.

--- index.js
+++ index.js
@@ -34,7 +34,12 @@ module.exports = function(options) {
       // This searches up from the specified package.json file, making sure
       // the config option behaves as expected. See issue #56.
       var searchFor = path.join('node_modules', name);
-      return require(findup(searchFor, {cwd: path.dirname(config)}));
+      var src = findup(searchFor, {cwd: path.dirname(config)});
+      if (src !== null) {
+      	return require(src);
+      } else {
+      	throw new Error('Cannot find `' + name + '` in your node_modules!');
+      }
     };
   } else {
     requireFn = require;
